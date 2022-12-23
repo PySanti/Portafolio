41fe8475d062784d4c9d4c5c6b50b50b228e142c
santiago <santiagodeandrade4@gmail.com>
2022-12-23 16:30:04 -0400

    Back color parcialmente seleccionado

--- src/Pages/Title/Title.jsx
+++ src/Pages/Title/Title.jsx
@@ -6,9 +6,7 @@ export function Title(){
     return (
     <header className="header-container">
         <h1 className="header-container__text title">
-            Hi, my name is  
             <span className="header-container__text__intro-text title__selected-text"> Santiago De Andrade </span> 
-            and this is my portafolio
         </h1>
     </header>
     )
