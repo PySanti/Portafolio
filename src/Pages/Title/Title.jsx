31a0eddd9fb9e3735da22a96372a411fad9beaa8
santiago <santiagodeandrade4@gmail.com>
2022-12-23 17:48:07 -0400

    Contenido del titulo modificado

--- src/Pages/Title/Title.jsx
+++ src/Pages/Title/Title.jsx
@@ -6,7 +6,9 @@ export function Title(){
     return (
     <header className="header-container">
         <h1 className="header-container__text title">
+            I am  
             <span className="header-container__text__intro-text title__selected-text"> Santiago De Andrade </span> 
+            and this is my portafolio
         </h1>
     </header>
     )
