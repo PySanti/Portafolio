8f81dcbb0fd9d8fb67e9c46c100064346b028e59
santiago <santiagodeandrade4@gmail.com>
2022-12-23 18:10:38 -0400

    Problema con la seleccion de map finalizado

--- src/Others/Map/func/activateMap.js
+++ src/Others/Map/func/activateMap.js
@@ -3,6 +3,11 @@ import { setMap } from "./setMap";
 
 
 function activateFirstMapItem(MAP_ITEM_LIST){
+    for (let i = 0; i < MAP_ITEM_LIST.length; i++ ){
+        if (MAP_ITEM_LIST[i].classList.contains(mapItemSelectedClassName)){
+            return false;
+        }
+    }
     MAP_ITEM_LIST[0].classList.add(mapItemSelectedClassName)
 }
 
