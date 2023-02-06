2eeab0dfc57491ca12653300909a94e926c4f872
santiago <santiagodeandrade4@gmail.com>
2023-02-06 11:44:02 -0400

    Modificacion de portafolio

--- src/Pages/Title/Title.jsx
+++ src/Pages/Title/Title.jsx
@@ -8,7 +8,7 @@ export function Title(){
         <h1 className="header-container__text title">
             I am  
             <span className="header-container__text__intro-text title__selected-text"> Santiago De Andrade </span> 
-            and this is my portafolio
+            and this is my portfolio
         </h1>
     </header>
     )
