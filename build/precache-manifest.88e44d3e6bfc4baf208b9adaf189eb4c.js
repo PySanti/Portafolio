30a7c1e42a66bcff456ad4427a0ec89d2ad86b54
santiago <santiagodeandrade4@gmail.com>
2024-11-16 11:06:36 -0400

    Modificacion para produccion

--- build/precache-manifest.88e44d3e6bfc4baf208b9adaf189eb4c.js
+++ build/precache-manifest.88e44d3e6bfc4baf208b9adaf189eb4c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9b172f4a7264d7d79dead197056cc776",
+    "revision": "a657642e5678c3c0331ca23fc3227153",
     "url": "/index.html"
   },
   {
-    "revision": "1d1f0d958dcf1fbc1cb5",
-    "url": "/static/css/main.450eed97.chunk.css"
+    "revision": "f7c2411731b7d8b6473e",
+    "url": "/static/css/main.eb8ad7b7.chunk.css"
   },
   {
     "revision": "3fa247b8906e29e86e1b",
     "url": "/static/js/2.878067d7.chunk.js"
   },
   {
-    "revision": "1d1f0d958dcf1fbc1cb5",
-    "url": "/static/js/main.7c67c301.chunk.js"
+    "revision": "f7c2411731b7d8b6473e",
+    "url": "/static/js/main.46f5c910.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.36d9014efab6fc50ce4a7f7e71fe584f.js"
+  "/precache-manifest.88e44d3e6bfc4baf208b9adaf189eb4c.js"
 );
 
 self.addEventListener('message', (event) => {
