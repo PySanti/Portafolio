1d0f75e59f2a1a7dcd7640d9c86f8c5328988e63
santiago <santiagodeandrade4@gmail.com>
2024-11-16 10:48:08 -0400

    Modificacion para produccion

---
+++ build/precache-manifest.36d9014efab6fc50ce4a7f7e71fe584f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b172f4a7264d7d79dead197056cc776",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d1f0d958dcf1fbc1cb5",
+    "url": "/static/css/main.450eed97.chunk.css"
+  },
+  {
+    "revision": "3fa247b8906e29e86e1b",
+    "url": "/static/js/2.878067d7.chunk.js"
+  },
+  {
+    "revision": "1d1f0d958dcf1fbc1cb5",
+    "url": "/static/js/main.7c67c301.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
